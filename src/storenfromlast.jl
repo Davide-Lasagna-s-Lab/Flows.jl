b3c71e6cd683183b5ac18261c89082d4ad253f4a
Thomas Burton <tburton5572@gmail.com>
2025-07-03 12:01:34 +0100

    small changes

--- src/storenfromlast.jl
+++ src/storenfromlast.jl
@@ -1,12 +1,14 @@
-export StoreNFromLast
-
 # A special monitor that only store the time and 
 # state N time steps before the last step is made.
 # The state should allow broadcasting assignment
+
+export StoreNFromLast
+
 mutable struct StoreNFromLast{N, X, F} <: AbstractMonitor{Float64, X}
     x::X
     f::F
     t::Float64
+
     function StoreNFromLast{N}(z, f::F = Base.copy) where {N, F}
         x = f(z)
         new{N, typeof(x), F}(x, f, 0.0)
